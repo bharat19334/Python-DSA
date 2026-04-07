3123c483ce1c1719c2de96555ba001f3a4f7299d
Bharat Goswami <bharatgoswami49200619@gmail.com>
2026-04-07 22:45:08 +0530

    initial commit

--- LeetcodeDSA/Move_zeros.py
+++ LeetcodeDSA/Move_zeros.py
@@ -1,5 +1,4 @@
-# moving all zero element in the last of all non zero element without using any extra space  
-
+# # 1st approach
 def Move_zeros(arr):
     insert_index = 0
     for i in range(0,len(arr)):
@@ -8,5 +7,20 @@ def Move_zeros(arr):
             insert_index += 1
     return arr
 arr = [1,0,5,0,6,0,4]
-
 print(Move_zeros(arr))
+
+# 2nd approach
+def Move_zeros(nums):
+    temp = [] * len(nums)
+    for i in range(0,len(nums)):
+        if nums[i] != 0:
+            temp.append(nums[i])
+    nums2 = len(temp)
+    for i in range(0,nums2):
+        nums[i] = temp[i]
+    for i in range(nums2,len(nums)):
+        nums[i] = 0
+    return nums
+nums = [0,1,2,5,6,0,0,0,1]
+print(Move_zeros(nums))
+

dcf0d24de6b3284f2df58660556f23a67a13d62f
Bharat Goswami <bharatgoswami49200619@gmail.com>
2026-04-09 13:32:24 +0530

    initial commit

--- LeetcodeDSA/missing_element.py
+++ LeetcodeDSA/missing_element.py
@@ -1,11 +1,11 @@
-# brute force solution
+# 1st approach
 nums = [3,0,1,4,2,6]
 n = len(nums)
 for i in range(0,n+1):
     if i not in nums:
         print(i)
         
-# optimal solution
+# 2nd approach
 def missing_number(nums):
     freq =  {}
     for i in range(0,len(nums)+1):
@@ -16,4 +16,14 @@ def missing_number(nums):
         if v == 0:
             return k
 nums = [0,1,2,3,5]
+print(missing_number(nums))
+
+# 3rd approach
+def missing_number(nums):
+    n = len(nums)
+    actual_sum = n*(n+1)/2
+    current_sum = sum(nums)
+    return int(actual_sum - current_sum)
+
+nums = [1,2,0,4,5,6]
 print(missing_number(nums))

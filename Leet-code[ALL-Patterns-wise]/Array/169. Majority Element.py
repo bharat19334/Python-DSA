7e24adc7f5a1b9b9c0da574bebcfec4ad17c4f42
Bharat Goswami <bharatgoswami49200619@gmail.com>
2026-07-01 00:48:18 +0530

    Added all DSA folders

---
+++ Leet-code[ALL-Patterns-wise]/Array/169. Majority Element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        for i in range(0,len(nums)):
+            count = 0
+            for j in range(0,len(nums)):
+                if nums[i]==nums[j]:
+                    count +=1
+            if count > len(nums)//2:
+                return nums[i]
+            else:
+                count = 1
+
+            
